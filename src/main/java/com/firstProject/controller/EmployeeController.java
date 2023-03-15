38463cff882d1f5912c67f196ad99a7c476ad73d
Black21396 <fadi.199620@gmail.com>
2023-03-15 12:51:45 +0100

    add cross origin annotation

--- src/main/java/com/firstProject/controller/EmployeeController.java
+++ src/main/java/com/firstProject/controller/EmployeeController.java
@@ -4,22 +4,18 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.firstProject.model.Employee;
 import com.firstProject.services.EmployeeService;
 
+
+// allow to angular to access to this services
+@CrossOrigin(origins = "http://localhost:4200")
+
 @RestController
 // define the url to our application (http://localhost:8008/api/employees)
 @RequestMapping("/api/employees")
-
 /*
  * This Controller(handles the HTTP requests) contain the Rest request (add,
  * delete, update, get)
